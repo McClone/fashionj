954b767d066c8d65e158e8812963f7e17762f58d
McClone <abc15606@163.com>
2016-06-24 14:15:50 +0800

    进一步完善

--- fashionwork/src/main/java/org/fashion/work/web/LoginController.java
+++ fashionwork/src/main/java/org/fashion/work/web/LoginController.java
@@ -1,42 +1,12 @@
 package org.fashion.work.web;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.fashion.work.shiro.web.AbstractLoginController;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhengsd
  */
 @RestController
-public class LoginController {
-
-    @GetMapping(value = "/index")
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping(value = "/login")
-    public String login() {
-        return "login";
-    }
-
-    @GetMapping(value = "/unauthorized")
-    public String unauthorized() {
-        return "unauthorized";
-    }
-
-    @PostMapping(value = "/login")
-    public ResponseEntity<String> doLogin(String username, String password, boolean rememberMe) {
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        token.setRememberMe(rememberMe);
-        subject.login(token);
-        return new ResponseEntity<>("login true", HttpStatus.OK);
-    }
+public class LoginController extends AbstractLoginController {
 
 }
