c410051d559145f48f7f2895450d9efa499d1666
McClone <abc15606@163.com>
2016-06-27 09:15:58 +0800

    增加模块引擎

---
+++ fashionwork/src/main/java/org/fashion/work/web/TemplateController.java
@@ -0,0 +1,21 @@
+package org.fashion.work.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author zhengsd
+ */
+@Controller
+public class TemplateController {
+
+    @RequestMapping("/doit")
+    public String doIt(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "/doit";
+    }
+
+}
